df3c5245785dadb2709f801f6b05de3bb36b3b76
pavels-s <pavels.sarkels@gmail.com>
2026-06-02 16:40:42 +0300

    Concatenation and interpolation

---
+++ lessons/lesson2.js
@@ -0,0 +1,10 @@
+//Concatination
+var price = 50
+var itemName = "Cup"
+var messageToPrint = "The price for your " + itemName + " is " + price + " dollars"
+console.log(messageToPrint)
+
+
+//Interpolation
+var messageToPrint2 = `The price for your ${itemName} is ${price} dollars`
+console.log(messageToPrint2)
