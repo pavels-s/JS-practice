c1fa09fe42495e65fb7239c6215627a7777d7c30
pavels-s <pavels.sarkels@gmail.com>
2026-06-02 16:26:15 +0300

    JS variables and data types

---
+++ lessons/lesson1.js
@@ -0,0 +1,24 @@
+//1. Hello World.
+console.log("Hello world!")
+
+// Variables
+var firstName = "John"
+let lastName = "Smith"
+console.log(firstName, lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age, sex)
+
+//constants cannot be changed later, and should be created at the beginning
+const occupation = "Engineer"
+console.log(occupation)
+
+//data types
+var middleName = "David"  //sting can be in '' and in ""
+var ageOfBorther = 25
+var isMarried = false
+var yearsInMarriage = null
+var numberOfCars = undefined
+
