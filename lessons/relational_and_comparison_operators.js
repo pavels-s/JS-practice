36cfa155fcbce05f8de0f8c2607a1e65e4a684b8
pavels-s <pavels.sarkels@gmail.com>
2026-06-03 09:37:46 +0300

    Relational and comparison operators

---
+++ lessons/relational_and_comparison_operators.js
@@ -0,0 +1,13 @@
+// > - more than
+// < - less than
+// >=0 - more than equal
+// <= - less than equal
+
+var result = 10 > 5
+console.log(result)
+
+
+//Equality operations
+var x = 1
+console.log(x == '1') // loose comparison, just comparing value, so it is true
+console.log(x === '1') // strict comparison. Not checking just value, but also a data type (string not equal to integer)
